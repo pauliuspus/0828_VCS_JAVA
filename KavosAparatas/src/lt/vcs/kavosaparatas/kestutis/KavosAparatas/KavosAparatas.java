613e9e868e174d8826142010bcd5f93fa94f9ba0
kestutis <kestutis_nutautas@hotmail.com>
2017-09-19 19:22:25 +0300

    rqekj

---
+++ KavosAparatas/src/lt/vcs/kavosaparatas/kestutis/KavosAparatas/KavosAparatas.java
@@ -0,0 +1,166 @@
+package KavosAparatas;
+
+import Produktai.Produktai;
+
+public class KavosAparatas {
+	
+	//int cukrus = 0;
+	//int kava = 0;
+	//int vanduo = 0;
+	private int panaudojimai = 0;
+	
+	public Produktai produktai;
+	
+	public KavosAparatas (int cukrus, int kava, int vanduo) {
+		/*this.cukrus = cukrus;
+		this.kava = kava;
+		this.vanduo = vanduo;*/
+		produktai = new Produktai(cukrus, kava, vanduo);
+	
+	}
+	
+	public void gaminkJuodaKava() {
+		
+		if (this.panaudojimai < 50) {
+			if (tikrinkArUztenka()) {
+				produktai.setCukrus(produktai.getCukrus() - 1);
+				produktai.setKava(produktai.getKava() - 1);
+				produktai.setVanduo(produktai.getVanduo() - 1);
+				this.panaudojimai ++; 
+				System.out.println("skanaus");
+			}
+			
+		}
+		else System.out.println("isvalyk aparata");
+		
+	}
+	
+public void gaminkEspresso() {
+		
+		if (this.panaudojimai < 50) {
+			if (tikrinkArUztenka()) {
+				produktai.setCukrus(produktai.getCukrus() - 1);
+				produktai.setKava(produktai.getKava() - 2);
+				produktai.setVanduo(produktai.getVanduo() - 1);
+				this.panaudojimai ++; 
+				System.out.println("skanaus");
+			}
+			
+		}
+		else System.out.println("isvalyk aparata");
+		
+	}
+
+public void gaminkCapuccino() {
+	
+	if (this.panaudojimai < 50) {
+		if (tikrinkArUztenka()) {
+			produktai.setCukrus(produktai.getCukrus() - 2);
+			produktai.setKava(produktai.getKava() - 1);
+			produktai.setVanduo(produktai.getVanduo() - 2);
+			this.panaudojimai ++; 
+			System.out.println("skanaus");
+		}
+		
+	}
+	else System.out.println("isvalyk aparata");
+	
+}
+	
+	private boolean tikrinkArUztenka() {
+					
+		if (produktai.getCukrus() < 2) { System.out.println("Neuztenka cukraus");
+		 return false;}
+		if (produktai.getKava() < 2) { System.out.println("Neuztenka kavos");
+		 return false;}
+		if (produktai.getVanduo() < 2) { System.out.println("Neuztenka vandens");
+		 return false;}
+		return true;
+		
+		}
+	
+	public void papildykCukraus () {
+		if (produktai.getCukrus() < 50) {
+		produktai.setCukrus(produktai.getCukrus() + 50);
+		System.out.println("Cukraus papildyta");
+		} else System.out.println("cukraus uztenka");
+			
+	}
+	
+	public void papildykPupeliu () {
+		if (produktai.getKava() < 50) {
+			produktai.setKava(produktai.getKava() + 50);
+			System.out.println("pupeliu papildyta");
+			} else System.out.println("pupeliu uztenka");
+				
+	}
+	
+	public void papildykVandens () {
+		if (produktai.getVanduo() < 50) {
+			produktai.setVanduo(produktai.getVanduo() + 50);
+			System.out.println("vandens papildyta");
+			} else System.out.println("vandens uztenka");
+		
+	}
+	
+	public void valykAparata() {
+		this.panaudojimai = 0;
+		System.out.println("Aparatas isvalytas");
+	}
+	
+	public void rodykBusena() {
+		System.out.println("cukraus " + produktai.getCukrus() + ", "
+				+ "kavos " + produktai.getKava() + ", Vandens " + produktai.getVanduo());
+		System.out.println("iki valymo liko " + (50 - getPanaudojimai()));
+	}
+	
+	
+
+
+
+	/*public int getCukrus() {
+		return cukrus;
+	}
+
+	public void setCukrus(int cukrus) {
+		this.cukrus = cukrus;
+	}
+
+	public int getKava() {
+		return kava;
+	}
+
+	public void setKava(int kava) {
+		this.kava = kava;
+	}
+
+	public int getVanduo() {
+		return vanduo;
+	}
+
+	public void setVanduo(int vanduo) {
+		this.vanduo = vanduo;
+	}*/
+
+	public int getPanaudojimai() {
+		return panaudojimai;
+	}
+
+	public void setPanaudojimai(int panaudojimai) {
+		this.panaudojimai = panaudojimai;
+	}
+
+	public Produktai getProduktai() {
+		return produktai;
+	}
+
+	public void setProduktai(Produktai produktai) {
+		this.produktai = produktai;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ KavosAparatas/src/lt/vcs/kavosaparatas/kestutis/Main/Main.java
@@ -0,0 +1,73 @@
+package Main;
+
+import java.util.Scanner;
+
+import KavosAparatas.KavosAparatas;
+
+public class Main {
+	
+	public static void pradekAparatoDarba() {
+		KavosAparatas aparatas = new KavosAparatas(0, 0, 0);
+		sukuriamMenu();
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			
+			int ivestas = scanner.nextInt();
+					
+			switch (ivestas) {
+			case 1:
+				 aparatas.gaminkJuodaKava();
+				 break;
+			case 2:
+				aparatas.gaminkEspresso();
+				break;
+			case 3:
+				aparatas.gaminkCapuccino();
+				break;
+			case 4:
+				aparatas.papildykCukraus();
+				break;
+			case 5:
+				aparatas.papildykPupeliu();
+				break;
+			case 6:
+				aparatas.papildykVandens();
+				break;
+			case 7:
+				aparatas.valykAparata();
+				break;
+			case 8:
+				aparatas.rodykBusena();
+				break;
+		
+				
+			default:
+				System.out.println("Neteisingas meniu punktas");
+				break;
+			}
+			scanner.close();
+		}
+		
+		
+	}
+	
+	public static void sukuriamMenu() {
+		System.out.println("1. Juoda kava");
+		System.out.println("2. Espresso");
+		System.out.println("3. Capuccino");
+		System.out.println("4. Papildyti cukraus");
+		System.out.println("5. Papildyti pupeliu");
+		System.out.println("6. Papildyti vandens");
+		System.out.println("7. Plauti");
+		System.out.println("8. Busena");
+	}
+
+	public static void main(String[] args) {
+		pradekAparatoDarba();
+		
+		
+		
+	}
+
+}
+
