6b45bdfb40421b8ed648386a223f8e84f721139c
kestutis <kestutis_nutautas@hotmail.com>
2017-10-03 20:03:16 +0300

    llll

---
+++ LaivuMusis/src/lt/vcs/laivumusis/piratai/MockZaidejas.java
@@ -0,0 +1,20 @@
+package lt.vcs.laivumusis.piratai;
+
+import lt.vcs.laivumusis.common.Zaidejas;
+import lt.vcs.laivumusis.common.Zaidimas;
+
+public class MockZaidejas implements Zaidejas {
+
+	@Override
+	public void run() {
+		System.out.println("kazakass");
+		
+	}
+
+	@Override
+	public Zaidimas getZaidimas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
