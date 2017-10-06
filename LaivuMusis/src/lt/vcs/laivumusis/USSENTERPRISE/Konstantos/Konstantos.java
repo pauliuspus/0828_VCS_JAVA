cee916b3017f90a23e27d2f2143cfa16b1cc5237
pauliuspus <pusvaskis@mgial.com>
2017-10-06 19:14:03 +0300

    konstantos

---
+++ LaivuMusis/src/lt/vcs/laivumusis/USSENTERPRISE/Konstantos/Konstantos.java
@@ -0,0 +1,22 @@
+package lt.vcs.laivumusis.USSENTERPRISE.Konstantos;
+
+public class Konstantos {
+
+    private Konstantos() {}
+
+
+    public static final int LEKTUVNESIS = 5;
+    public static final int KOVINIS_LAIVAS = 4;
+    public static final int KRUIZERIS = 3;
+    public static final int POVANDENINIS = 3;
+    public static final int NAIKINTOJAS = 2;
+    public static final int ZAIDEJO_GYVYBES =17;
+
+    public static final String LAIVO_IKONA = "X";
+    public static final String LENTOS_IKONA = "-";
+    public static final String LAIVAS_PATAIKYTA = "P";
+    public static final String LAIVAS_NEPATAIKYTA = "N";
+
+ //   public static final char[] ZAIDIMO_LENTOS_RAIDES = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+   // public static final int LENTOS_DYDIS = 10;
+}
